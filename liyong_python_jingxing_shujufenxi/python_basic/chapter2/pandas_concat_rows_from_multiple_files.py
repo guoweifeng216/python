1513c48b5969391aa107e9c9b53f4487500ed251
weifeng.guo <weifeng.guo@cnexlabs.com>
2019-01-04 17:30:48 +0800

    modify some

--- liyong_python_jingxing_shujufenxi/python_basic/chapter2/pandas_concat_rows_from_multiple_files.py
+++ liyong_python_jingxing_shujufenxi/python_basic/chapter2/pandas_concat_rows_from_multiple_files.py
@@ -18,5 +18,9 @@ all_data_frames = []
 for file in all_files:
     data_frame = pd.read_csv(file, index_col=None)
     all_data_frames.append(data_frame)
+print(all_data_frames[0])
+print(all_data_frames[1])
+print(all_data_frames[2])
 data_frame_concat = pd.concat(all_data_frames, axis=0, ignore_index=True)
+print(data_frame_concat)
 data_frame_concat.to_csv(output_file, index=False)

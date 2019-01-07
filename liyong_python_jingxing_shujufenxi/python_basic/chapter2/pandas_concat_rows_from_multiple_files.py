f9194f9a5822bfc0c095212b55bdd49c7658788f
weifeng.guo <weifeng.guo@cnexlabs.com>
2019-01-07 16:07:32 +0800

    modify some testcase

--- liyong_python_jingxing_shujufenxi/python_basic/chapter2/pandas_concat_rows_from_multiple_files.py
+++ liyong_python_jingxing_shujufenxi/python_basic/chapter2/pandas_concat_rows_from_multiple_files.py
@@ -18,9 +18,11 @@ all_data_frames = []
 for file in all_files:
     data_frame = pd.read_csv(file, index_col=None)
     all_data_frames.append(data_frame)
-print(all_data_frames[0])
-print(all_data_frames[1])
-print(all_data_frames[2])
-data_frame_concat = pd.concat(all_data_frames, axis=0, ignore_index=True)
+# print(all_data_frames[0])
+# print(all_data_frames[1])
+# print(all_data_frames[2])
+data_frame_concat = pd.concat([all_data_frames[0],all_data_frames[1],all_data_frames[2]], axis=0, sort=True, ignore_index=True)
+"""# 这段代码垂直堆叠数据框。如果你需要平行连接数据，那么就在 concat 函数中设置
+axis=1 """
 print(data_frame_concat)
 data_frame_concat.to_csv(output_file, index=False)

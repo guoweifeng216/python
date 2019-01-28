4ff074cf5df06989721b155f3c10dba8228f9b5e
weifeng.guo <weifeng.guo@cnexlabs.com>
2019-01-28 09:32:54 +0800

    add chapter6 testcase

---
+++ liyong_python_jingxing_shujufenxi/python_basic/chapter6/bar_plot.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+
+@athor:weifeng.guo 
+@data:2019/1/25 9:00
+@filename:bar_plot
+
+"""
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+customers = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO']
+customers_index = range(len(customers))
+sale_amounts = [127, 90, 201, 111, 232]
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+ax1.bar(customers_index, sale_amounts, align='center', color='darkblue')
+ax1.xaxis.set_ticks_position('bottom')
+ax1.yaxis.set_ticks_position('left')
+plt.xticks(customers_index, customers, rotation=0, fontsize='small')
+plt.xlabel('Customer Name')
+plt.ylabel('Sale Amount')
+plt.title('Sale Amount per Customer')
+plt.savefig('bar_plot.png', dpi=400, bbox_inches='tight')
+plt.show()
---
+++ liyong_python_jingxing_shujufenxi/python_basic/chapter6/seaborn_plots.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+
+@athor:weifeng.guo 
+@data:2019/1/25 13:56
+@filename:seaborn_plots
+
+"""

149f8abe7087a59bb888f3617520749ca4b9cf15
weifeng.guo <weifeng.guo@cnexlabs.com>
2019-02-20 16:34:08 +0800

    modify some testcase

--- liyong_python_jingxing_shujufenxi/python_basic/chapter2/10csv_reader_sum_average_from_multiple_files.py
+++ liyong_python_jingxing_shujufenxi/python_basic/chapter2/10csv_reader_sum_average_from_multiple_files.py
@@ -13,11 +13,12 @@ import os
 import sys
 input_path = sys.argv[1]
 output_file = sys.argv[2]
-output_header_list = ['filename', 'total_sales', 'average_sales']
-csv_out_file = open(output_file, 'a', newline='')
+# output_header_list = ['filename', 'total_sales', 'average_sales']
+output_header_list = ['filename', 'Total MBs per Second', 'average']
+csv_out_file = open(output_file, 'w', newline='')
 filewriter = csv.writer(csv_out_file)
 filewriter.writerow(output_header_list)
-for input_file in glob.glob(os.path.join(input_path,'supplier*')):
+for input_file in glob.glob(os.path.join(input_path,'128K_Seq_Reads_QD128_2H.log*')):
     with open(input_file, 'r',newline='') as csv_in_file:
         filereader = csv.reader(csv_in_file)
         output_list = []
@@ -26,12 +27,14 @@ for input_file in glob.glob(os.path.join(input_path,'supplier*')):
         total_sales = 0.0
         number_of_sales = 0.0
         for row in filereader:
-            sale_amount = row[3]
-            total_sales += float(str(sale_amount).strip('$').replace(',', ''))
+            sale_amount = row[1]
+            total_sales += float(str(sale_amount))
+            # total_sales += float(str(sale_amount).strip('$').replace(',', ''))
             number_of_sales += 1
 
         average_sales = '{0:.2f}'.format(total_sales / number_of_sales)
         output_list.append(total_sales)
         output_list.append(average_sales)
+        print(output_list)
         filewriter.writerow(output_list)
 csv_out_file.close()
--- liyong_python_jingxing_shujufenxi/python_basic/chapter2/rtu_server.py
+++ liyong_python_jingxing_shujufenxi/python_basic/chapter2/rtu_server.py
@@ -7,3 +7,35 @@
 @filename:rtu_server
 
 """
+from serial import Serial
+from collections import defaultdict
+
+from umodbus.server.serial import get_server
+from umodbus.server.serial.rtu import RTUServer
+
+s = Serial('com3')
+s.timeout = 10
+
+data_store = defaultdict(int)
+app = get_server(RTUServer, s)
+
+
+@app.route(slave_ids=[1], function_codes=[1, 2], addresses=list(range(0, 10)))
+def read_data_store(slave_id, function_code, address):
+    """" Return value of address. """
+    print(data_store[address])
+    return data_store[address]
+
+
+@app.route(slave_ids=[1], function_codes=[5, 15], addresses=list(range(0, 10)))
+def write_data_store(slave_id, function_code, address, value):
+    """" Set value for address. """
+    data_store[address] = value
+
+if __name__ == '__main__':
+
+    try:
+        app.serve_forever()
+        read_data_store()
+    finally:
+        app.shutdown()
